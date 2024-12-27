44bef3381128cbeadfee24f9b5865add3708bc1d
Rami <138556019+rami242424@users.noreply.github.com>
2024-12-27 11:06:12 +0900

    #2.1 Our First Styled Component

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // root를 생성
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
