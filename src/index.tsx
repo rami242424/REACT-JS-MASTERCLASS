16d45e2a247ad7bd0734b943bea669dd3985f78a
Rami <138556019+rami242424@users.noreply.github.com>
2024-12-28 21:34:42 +0900

    root edit

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = (ReactDOM as any).createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
